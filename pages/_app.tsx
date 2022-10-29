443aeb0cd741ef2bd6c6dc2ea555da9307a087e6
Sudhanshu Tripathi <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-10-29 22:56:08 +0530

    Added theme file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* 
+      CssBaseline adds Normalize CSS. It makes page to follow mui. 
+      It adds CSS properties like "box-sizing: border-box" and "margin : 0"
+      */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
