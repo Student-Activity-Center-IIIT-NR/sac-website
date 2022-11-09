fc0e22bc9547a6ab5fab8ec966ac86a1f537a08e
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-11-10 02:39:06 +0530

    Page scaling based on viewport

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,10 @@ const Home: NextPage = () => {
           name="description"
           content="Student Activity Center IIIT Naya Raipur | SAC | SAC IIIT Naya Raipur"
         />
+        <meta name="viewport" content="width=1200, minimum-scale=0.25" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <BaseLayout>
         <News />
       </BaseLayout>
