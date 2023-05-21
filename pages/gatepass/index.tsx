51e23576162e8dd3f8d5b9612b5a8f700dcc9158
Sudhanshu Tripathi <sudhanshutripathi008@gmail.com>
2023-05-21 23:39:52 +0530

    Policy (#104)
    * gatepass Landing Page
    
    * added privacy policy for gatepass
    
    ---------
    
    Co-authored-by: Deepesh76780 <da76780@gmail.com>
    Co-authored-by: sac-iiitnr <115001496+sac-iiitnr@users.noreply.github.com>

---
+++ pages/gatepass/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { NextPage } from "next";
+import GatePassLayout from "../../layouts/GatePassLayout";
+import LandingPage from "../../features/getpass/LandingPage";
+
+const Gatepass: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>SAC | GATEPASS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GatePassLayout>
+        <LandingPage />
+      </GatePassLayout>
+    </>
+  );
+};
+
+export default Gatepass;
