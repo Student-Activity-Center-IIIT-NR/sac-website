29606c059977b59579e92f97addefb39f0df11d2
AB527 <atharvabedekar527@gmail.com>
2025-08-17 01:10:11 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'sac-website'; // ← replace with your repo name
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
+  basePath: "/",
+  assetPrefix: "/",
 };
 
 module.exports = nextConfig;
